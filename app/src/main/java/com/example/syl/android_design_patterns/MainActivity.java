136fc2eb2847a8e822dd5e2e994b1b90dcb1e236
Sylvia Domenech <s4domenech@gmail.com>
2018-04-04 14:56:14 +0200

    Example builder

--- app/src/main/java/com/example/syl/android_design_patterns/MainActivity.java
+++ app/src/main/java/com/example/syl/android_design_patterns/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.syl.android_design_patterns;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.syl.android_design_patterns.builder_example.User;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        User user = new User.UserBuilder("Pitt", "Smith").age(24).phone("+34000000000").address("Address").build();
+
+        System.out.println(user.toString());
     }
 }
